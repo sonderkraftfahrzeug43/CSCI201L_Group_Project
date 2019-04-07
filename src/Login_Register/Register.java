4a786571d6e1d504f93a13fc1ff448bd8ddcfebb
Christoph Wiederin <wiederin@usc.edu>
2019-04-07 16:41:30 -0700

    Add files via upload

---
+++ src/Login_Register/Register.java
@@ -0,0 +1,82 @@
+package Login_Register;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.TimeZone;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Register
+ */
+@WebServlet("/Register")
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ResultSet rs = null;
+		String nextPage = null;
+		Statement st = null;
+		Connection conn = null;
+		try {
+			String username = request.getParameter("username");
+			String password = request.getParameter("password");
+			String sql = "insert into user(username,password) values(?,?)";
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/schedulebuilder?serverTimezone=" + TimeZone.getDefault().getID();
+			conn = DriverManager.getConnection(url,"root","yourpassword");
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM user WHERE userName='" + username + "'");
+			if(rs.next())
+			{
+				request.setAttribute("utError", "User is taken");
+				nextPage ="/registration.jsp";
+			}
+			else
+			{
+				PreparedStatement ps = conn.prepareStatement(sql);
+				ps.setString(1, username);
+				ps.setString(2, password);
+				ps.executeUpdate();
+				nextPage ="/main.jsp";					
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextPage);
+		dispatcher.forward(request, response);
+}
+}
