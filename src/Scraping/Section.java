967edb809bce4a8bf2dfe4b8a5807ad0d0562111
Mark Scarsi <scarsi@usc.edu>
2019-04-16 23:20:42 -0700

    added a getInfo method

--- src/Scraping/Section.java
+++ src/Scraping/Section.java
@@ -33,7 +33,11 @@ public class Section {
 		
 		
 	}
-	
+	String getInfo() {
+		String returnable = sectionType + ", " + sectionID + ", " 
+				+ time.getFormattedTime() + ", " + professor + ", " + location;
+		return returnable;
+	}
 	void printInfo() {
 		System.out.println(sectionType + ", " + sectionID + ", " 
 				+ time.getFormattedTime() + ", " + professor + ", " + location);
