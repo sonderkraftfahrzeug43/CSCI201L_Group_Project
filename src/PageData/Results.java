3ca2a114517543de0abc9db985299a2a60d0480a
Mark Scarsi <scarsi@usc.edu>
2019-04-16 22:54:37 -0700

    Results Progress

--- src/PageData/Results.java
+++ src/PageData/Results.java
@@ -1,11 +1,14 @@
-package PageData;
+ package PageData;
 
 import java.io.IOException;
+import Scraping.*;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/Results")
 public class Results extends HttpServlet {
@@ -18,7 +21,45 @@ public class Results extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		String choice = request.getParameter("option");
+		HttpSession session = request.getSession();
+		String search = "";
+		String nextPage = "/results.jsp";
+		System.out.println("NEW REQUEST");
+		if(choice.equals("Class")){
+			System.out.println("CODE FOR CLASS");
+			search = request.getParameter("friendText");
+			if (search != null){
+				Data data = (Data)session.getAttribute("data");
+				
+			}
+			else{
+				nextPage = ("/profile.jsp");
+			}
+		}
+		if(choice.equals("Friend")){
+			System.out.println("CODE FOR FRIEND");
+			search = request.getParameter("friendText");
+			if (search != null){
+				
+			}
+			else{
+				nextPage = ("/profile.jsp");
+			}
+		}
+		if(choice.equals("Professor")){
+			System.out.println("CODE FOR PROFESSOR");
+			search = request.getParameter("professorText");
+			if (search != null){
+				
+			}
+			else{
+				nextPage = ("/profile.jsp");
+			}
+		}
+		
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextPage);
+		dispatcher.forward(request, response);
 	}
 
 }
