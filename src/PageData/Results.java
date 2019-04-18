eec4a2918ca6d07e7eb20a7d136cd8467f652e5f
Mark Scarsi <scarsi@usc.edu>
2019-04-17 21:47:37 -0700

    Added extra capability

--- src/PageData/Results.java
+++ src/PageData/Results.java
@@ -28,7 +28,65 @@ public class Results extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
+	public int getNumFriends(String className,HttpSession session){
+		int sharedClasses = 0;
+		String username = session.getAttribute("UserID").toString();
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		PreparedStatement ps1 = null;
+		ResultSet rs1 = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/schedulebuilder?serverTimezone=" + TimeZone.getDefault().getID();
+			conn = DriverManager.getConnection(url , "root" , "roottest");
+			ps = conn.prepareStatement("SELECT * FROM follow WHERE user1ID=?");
+			ps.setString(1, username);
+			rs = ps.executeQuery();
+			Vector<String> friends = new Vector<String>();
+			while(rs.next()) {
+				String friendID = rs.getString("user2ID");
+				friends.add(friendID);
+			}
+			for (int index = 0; index < friends.size(); index++){
+				ps1 = null;
+				rs1 = null;
+				ps1 = conn.prepareStatement("SELECT * FROM currentclass WHERE userID=?");
+				ps1.setString(1, friends.get(index));
+				rs1 = ps1.executeQuery();
+				while(rs1.next()){
+					if (className.equals(rs1.getString("name"))){
+						sharedClasses++;
+					}
+				}
+			}
+		} catch(ClassNotFoundException cnfe) {
+			System.out.println("cnfe:" + cnfe.getMessage());
+		} catch (SQLException sqle) {
+			System.out.println("sqle: " + sqle.getMessage());
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(ps != null) {
+					ps.close();
+				}
+				if(rs1 != null) {
+					rs.close();
+				}
+				if(ps1 != null) {
+					ps.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(SQLException sqle) {
+				System.out.println("sqle: " + sqle.getMessage());
+			}
+		}
+		return sharedClasses;
+	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String choice = request.getParameter("option");
 		Vector<String> results = new Vector<String>();
@@ -42,14 +100,21 @@ public class Results extends HttpServlet {
 			if (search != null){
 				System.out.println(search);
 				Data data = (Data)session.getAttribute("data");
-				Vector<Course> courses = Data.findCourses(search);
+				Vector<Course> courses = data.findCourses(search);
 				if (courses.size()==0){
 					results.add("No results available");
 				}
 				else{
-						results.add(courses.get(0).getHeader());
-						for (int index = 0; index < courses.get(0).getSections().size();index++){
-							results.addElement(courses.get(0).getSections().get(index).getInfo());
+					System.out.println("courses size" + courses.size());
+					for (int outer = 0; outer < courses.size();outer++){
+						for (int inner = 0; inner < courses.get(outer).getSections().size();inner++){
+							String name = courses.get(outer).name;
+							String title = courses.get(outer).title;
+							String completeName = name + " " + title;
+							results.add(courses.get(outer).getHeader() + " " + courses.get(outer).getSections().get(inner).getInfo());
+							System.out.println(getNumFriends(completeName,session));
+							System.out.println(courses.get(outer).getHeader() + " " +courses.get(outer).getSections().get(inner).getInfo());
+						}
 					}
 				}
 			session.setAttribute("resultsArray", results);	
@@ -152,20 +217,8 @@ public class Results extends HttpServlet {
 				nextPage = ("/profile.jsp");
 			}
 		}
-		if(choice.equals("Professor")){
-			System.out.println("CODE FOR PROFESSOR");
-			search = request.getParameter("professorText");
-			if (search != null){
-				
-			}
-			else{
-				nextPage = ("/profile.jsp");
-			}
-		}
-		
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextPage);
 		dispatcher.forward(request, response);
 	}
-
 }
 
