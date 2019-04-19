54b4bc2c335114e68970cf9504f832ec33665cab
Viet Truong <vqtruong@usc.edu>
2019-04-18 17:03:26 -0700

    Cleaning up code

--- src/Scraping/Schedule.java
+++ src/Scraping/Schedule.java
@@ -5,7 +5,7 @@ public class Schedule {
 	// Example: times[120][4] represents 12:00PM on Thursday
 	// Row 0 is 12:00AM
 	// Col 0 is Monday
-	private boolean[][] times = new boolean[241][7];
+	public boolean[][] times = new boolean[241][7];
 	Vector<Section> sections = new Vector<Section>();
 	
 	Schedule() {
@@ -16,6 +16,10 @@ public class Schedule {
 		}
 	}
 	
+	
+	// Copy s2 to s
+	
+	
 	@SuppressWarnings("unchecked")
 	Schedule(Schedule s) {
 		System.out.println("Cloning");
@@ -24,13 +28,15 @@ public class Schedule {
 	}
 	
 	public Schedule addSection(Section s)  {
-		if (this.conflicts(s)) return null;
+		if (this.conflicts(s)) {
+			return null;
+		}
 		
 		Time t = s.time;
 		boolean[] days = s.days;
 //		System.out.println(t.militaryStart + ", " + t.militaryEnd);
 //		System.out.println(s.sectionType + " - " + s.sectionID);
-		System.out.println(t.startTime + " ,  " + t.endTime);
+//		System.out.println(t.startTime + " ,  -" + t.endTime);
 		for (int i = t.militaryStart/10; i < t.militaryEnd/10; i++) {
 			for (int j = 0; j < days.length; j++) {
 				// If the class meets on that day and
@@ -69,17 +75,17 @@ public class Schedule {
 		
 		
 		
-		for (int i = t.militaryStart/10; i < 240; i++) {
+		for (int i = t.militaryStart/10; i < t.militaryEnd/10; i++) {
 			for (int j = 0; j < days.length; j++) {
 				// If the class meets on that day and 
 				if (days[j] == true && times[i][j] == true) {
+//					System.out.println(s.sectionID + " conflicts on day " + j + " during time " + i);
 					return true;
 				}
-					
 			}
 		}
 	
-		
+//		System.out.println("No conflict.");
 		return false;
 		
 	}
