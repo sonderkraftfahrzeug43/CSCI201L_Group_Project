661a1a1000fc2ddb20437eb2ec0150a0ecaa28d9
Christoph Wiederin <wiederin@usc.edu>
2019-04-21 23:38:21 -0700

    Create Server.java

---
+++ src/Threading/Server.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class Server {
+
+	private Vector<ServerThread> serverThreads;
+	public Server(int port) {
+		try {
+			System.out.println("Binding to port " + port);
+			ServerSocket ss = new ServerSocket(port);
+			System.out.println("Bound to port " + port);
+			
+			serverThreads = new Vector<ServerThread>();
+			while(true) {
+				Socket s = ss.accept(); // blocking
+				System.out.println("Connection from: " + s.getInetAddress());
+				ServerThread st = new ServerThread(s, this);
+				serverThreads.add(st);
+			}
+		} catch (IOException ioe) {
+			System.out.println("ioe in Server constructor: " + ioe.getMessage());
+		}
+	}
+	
+	public void broadcast(String message, ServerThread st) {
+		if (message != null) {
+			System.out.println(message);
+			for(ServerThread threads : serverThreads) {
+				if (st != threads) {
+					threads.sendMessage(message);
+				}
+			}
+		}
+	}
+	
+	public static void main(String [] args) {
+		Server svr = new Server(3456);
+	}
+}
