c005d3d8fd39c7c231376c1efcd3ee6548ee08a5
Mark Scarsi <scarsi@usc.edu>
2019-04-21 21:01:20 -0700

    Login.java setting session attributes

--- src/Login_Register/Login.java
+++ src/Login_Register/Login.java
@@ -3,11 +3,11 @@
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.TimeZone;
+import java.util.Vector;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,6 +16,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import Scraping.Section;
 
 /**
  * Servlet implementation class Login
@@ -29,6 +31,7 @@ public class Login extends HttpServlet {
      * @see HttpServlet#HttpServlet()
      */
 
+	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String userName = request.getParameter("username");
 		String password = request.getParameter("password");
@@ -36,44 +39,59 @@ public class Login extends HttpServlet {
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
+		Statement stMaj = null;
+		Statement stMin = null;
+		Statement stGrad = null;
+		ResultSet rsMaj = null;
+		ResultSet rsMin = null;
+		ResultSet rsGrad = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			String url = "jdbc:mysql://us-cdbr-iron-east-02.cleardb.net:3306/heroku_f034524e641ba65?serverTimezone=" + TimeZone.getDefault().getID();
 			conn = DriverManager.getConnection(url , "b8c39ba9e35da7" , "ebcfebb1");
-			st = conn.createStatement();
-			//st.executeUpdate("UPDATE `pageVisited` SET `count` = `count`+1 WHERE userID=4 and pageid=1)
-			rs = st.executeQuery("SELECT * FROM user WHERE userName='" + userName + "'");
-			boolean username_exists = false; 
-			boolean username_and_password = false; 
+			st = conn.createStatement();	
 			HttpSession session1 = request.getSession(true);
-			if(rs.next())
-			{
-				// wrong password
-				username_exists = true; 
-				request.setAttribute("wpError", "Wrong password");
-			}	
-			st = conn.createStatement();
-			//st.executeUpdate("UPDATE `pageVisited` SET `count` = `count`+1 WHERE userID=4 and pageid=1)
 			rs = st.executeQuery("SELECT * FROM User WHERE userName='" + userName + "' and pass= '"+password+"'");
 			if(rs.next()) 
-			{
-				// login
+				{
 				String iduser = rs.getString("UserID");
 				String idmajor = rs.getString("majorID");
 				String idminor = rs.getString("minorID");
 				String idgradyear = rs.getString("gradYID");
-				username_and_password = true;
 				nextPage = "/main.jsp";
 				session1.setAttribute("userName", userName);
 				session1.setAttribute("UserID", iduser);
 				session1.setAttribute("majorID", idmajor);
 				session1.setAttribute("minorID", idminor);
 				session1.setAttribute("gradYID", idgradyear);
-				
+				stGrad = conn.createStatement();
+				stMaj = conn.createStatement();
+				stMin = conn.createStatement();
+				rsMaj = stMaj.executeQuery("SELECT * FROM Major WHERE MajorID='" + idmajor + "'");
+				rsMin = stMin.executeQuery("SELECT * FROM Minor WHERE MinorID='" + idminor + "'");
+				rsGrad = stGrad.executeQuery("SELECT * FROM gradyear WHERE GradYID='" + idgradyear + "'");
+				rsMaj.next();
+				rsMin.next();
+				rsGrad.next();
+				String majorName = rsMaj.getString("name");
+				String majURL = rsMaj.getString("requirements");
+				String minorName = rsMin.getString("name");
+				String minURL = rsMin.getString("requirements");
+				String gradYearName = rsGrad.getString("year");
+				Vector<Section> currClasses = new Vector<Section>();
+				Vector<Section> prevClasses = new Vector<Section>();
+				Vector<String> friends = new Vector<String>();
+				session1.setAttribute("friends", friends);
+				session1.setAttribute("majURL", majURL);
+				session1.setAttribute("minURL",minURL);
+				System.out.println("Curr classes" + currClasses);
+				session1.setAttribute("currClasses", currClasses);
+				session1.setAttribute("prevClasses", prevClasses);
+				session1.setAttribute("gradYear", gradYearName);
+				session1.setAttribute("minorName", minorName);
+				session1.setAttribute("majorName", majorName);	
 			}
-			
-			if(!username_exists) {
-				// user does not exist
+			else {
 				request.setAttribute("nuError", "User does not exist");
 				nextPage = "/login.jsp";
 			}
