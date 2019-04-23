cd14c36489cad46fe94bf027e002737d9dffd511
Mark Scarsi <scarsi@usc.edu>
2019-04-23 07:04:49 -0700

    Scheduling

--- src/Scraping/Scheduling.java
+++ src/Scraping/Scheduling.java
@@ -1,16 +1,15 @@
 package Scraping;
-import java.util.Scanner;
 import java.util.Vector;
 
 public class Scheduling {
-	private static Vector<Vector<Section>> all = new Vector<Vector<Section>>();
+	private Vector<Vector<Section>> all = new Vector<Vector<Section>>();
 	private static Vector<Course> courses = new Vector<Course>();
 	
 	public static void testSearch() {
 		Data d = new Data();
 		while (true) {
 			String search = "math";
-			Vector<Course> c = d.findCourses(search);
+			Vector<Course> c = Data.findCourses(search);
 			for (int i = 0; i < c.size(); i++) {
 				c.get(i).printInfo();
 			}
@@ -21,11 +20,11 @@ public class Scheduling {
 	public static void testSchedule() {
 		Data d = new Data();
 
-		Course c1 = d.findCourses("CTIN-488").get(0);
-		Course c2 = d.findCourses("CSCI-353").get(0);
-		Course c3 = d.findCourses("CSCI-420").get(0);
-		Course c4 = d.findCourses("ITP-250").get(0);
-		Course c5 = d.findCourses("CSCI-350").get(0);
+		Course c1 = Data.findCourses("CTIN-488").get(0);
+		Course c2 = Data.findCourses("CSCI-353").get(0);
+		Course c3 = Data.findCourses("CSCI-420").get(0);
+		Course c4 = Data.findCourses("ITP-250").get(0);
+		Course c5 = Data.findCourses("CSCI-350").get(0);
 
 		Vector<Course> desiredCourses = new Vector<Course>();
 		desiredCourses.add(c1);
@@ -35,7 +34,7 @@ public class Scheduling {
 		desiredCourses.add(c5);
 		
 		
-		Vector<Vector<Section>> maybe = schedule(desiredCourses);
+		//Vector<Vector<Section>> maybe = schedule(desiredCourses);
 //		for (int i = 0; i < maybe.size(); i++) {
 //			System.out.println("NEW SCHEDULE: ");
 //			for (int j = 0; j < maybe.get(i).size(); j++) {
@@ -43,7 +42,7 @@ public class Scheduling {
 //			}
 //		}
 		
-		System.out.println(maybe.size());
+		//System.out.println(maybe.size());
 	}
 	
 	
@@ -54,9 +53,9 @@ public class Scheduling {
 		testSchedule();
 	}
 	
-	public static Vector<Vector<Section>> schedule(Vector<Course> _courses) {
+	public Vector<Vector<Section>> schedule(Vector<Course> _courses) {
 		courses.clear();
-		 
+		all.clear();
 		for (int i = 0; i < _courses.size(); i++) {
 			Vector<Vector<Section>> sectionType = _courses.get(i).sections;
 			for (int j = 0; j < sectionType.size(); j++) {
@@ -103,7 +102,7 @@ public class Scheduling {
 		}
 	}
 	
-	private static void scheduleHelper(int index, Schedule _schedule) {		
+	private void scheduleHelper(int index, Schedule _schedule) {		
 		// If you reach the end, do nothing
 		if (index == courses.size()) {
 			Schedule schedule = new Schedule();
